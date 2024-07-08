76a6e28c916e24ee9951460097d7d8dd2f9d8393
Francisco Uncal <franciscouncal@Fran-MacBook-Pro.local>
2024-07-07 23:09:10 -0300

    v.1.1

--- front/js/script.js
+++ front/js/script.js
@@ -1,34 +1,66 @@
-document.getElementById("payButton").addEventListener("click", function () {
-  document.getElementById("paymentInfo").classList.toggle("hidden");
-});
-
-// Esta es la función que será llamada al hacer clic en el botón
-function showAlertAndCloseModal() {
-  Swal.fire({
-    text: "Muchas Gracias por tu Regalo!",
-    icon: "success",
-    showConfirmButton: false,
-    iconColor: "#dbc0cf",
-    timer: 2000,
-  });;
-  closeModalC();
-}
-
-// Función para cerrar el modal
-function closeModalC() {
-  document.getElementById('myModal').style.display = 'none';
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const cartButton = document.getElementById("cart-button");
   const cartModal = document.getElementById("cart-modal");
   const closeCart = document.getElementById("close-cart");
+  const acceptAndCloseButton = document.getElementById("acceptAndCloseButton");
   const cartCount = document.getElementById("cart-count");
   const cartItemsContainer = document.getElementById("cart-items");
   const totalAmount = document.getElementById("total-amount");
-
   let cart = [];
   let products = [];
+
+  document.getElementById("payButton").addEventListener("click", function () {
+    document.getElementById("paymentInfo").classList.toggle("hidden");
+  });
+
+  // Función que será llamada al hacer clic en el botón "Aceptar y cerrar"
+  function showAlertAndCloseModal() {
+    Swal.fire({
+      text: "Muchas Gracias por tu Regalo!",
+      icon: "success",
+      showConfirmButton: false,
+      iconColor: "#dbc0cf",
+      timer: 3000,
+    }).then(() => {
+      clearCart();
+      closeModalC();
+    });
+  }
+
+  // Función para cerrar el modal
+  function closeModalC() {
+    document.getElementById("cart-modal").style.display = "none";
+  }
+
+  // Función para vaciar el carrito
+  function clearCart() {
+    cart = [];
+    updateCart();
+  }
+
+  // Función para actualizar el carrito
+  function updateCart() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+    cart.forEach((item) => {
+      const itemElement = document.createElement("div");
+      itemElement.classList.add("cart-item");
+      itemElement.innerHTML = `
+        <span>${item.name} - ${item.price.toLocaleString("es-AR", {
+        style: "currency",
+        currency: "ARS",
+      })} x ${item.quantity}</span>
+        <span class="close" onclick="removeFromCart(${item.id})">&times;</span>
+      `;
+      cartItemsContainer.appendChild(itemElement);
+      total += item.price * item.quantity;
+    });
+    totalAmount.textContent = total.toLocaleString("es-AR", {
+      style: "currency",
+      currency: "ARS",
+    });
+    cartCount.textContent = cart.length;
+  }
 
   // Fetch products from JSON
   fetch("data/products.json")
@@ -45,51 +77,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const productElement = document.createElement("div");
       productElement.classList.add("product");
       productElement.innerHTML = `
-                  <img src=${product.image}>
-                  <h3>${product.name}</h3>
-                  <p>${product.price.toLocaleString("es-AR", {
-                    style: "currency",
-                    currency: "ARS",
-                  })}</p>
-                  <button onclick="addToCart(${
-                    product.id
-                  })">Agregar Regalo</button>
-              `;
+        <img src=${product.image}>
+        <h3>${product.name}</h3>
+        <p>${product.price.toLocaleString("es-AR", {
+          style: "currency",
+          currency: "ARS",
+        })}</p>
+        <button onclick="addToCart(${product.id})">Agregar Regalo</button>
+      `;
       cardContent.appendChild(productElement);
     });
-  }
-
-  // Agregar al carrito y Mostrar
-  function updateCart() {
-    cartItemsContainer.innerHTML = "";
-    let total = 0;
-    cart.forEach((item) => {
-      const itemElement = document.createElement("div");
-      itemElement.classList.add("cart-item");
-      itemElement.innerHTML = `
-                  <span>${item.name} - ${item.price.toLocaleString("es-AR", {
-        style: "currency",
-        currency: "ARS",
-      })} x ${item.quantity}</span>
-                  <span class="close" onclick="removeFromCart(${
-                    item.id
-                  })">&times;</span>
-              `;
-      cartItemsContainer.appendChild(itemElement);
-      total += item.price * item.quantity;
-      Swal.fire({
-        text: "Regalo agregado al Carrito",
-        icon: "success",
-        showConfirmButton: false,
-        iconColor: "#dbc0cf",
-        timer: 2000,
-      });
-    });
-    totalAmount.textContent = total.toLocaleString("es-AR", {
-      style: "currency",
-      currency: "ARS",
-    });
-    cartCount.textContent = cart.length;
   }
 
   window.addToCart = function (id) {
@@ -101,6 +98,13 @@ document.addEventListener("DOMContentLoaded", () => {
       cart.push({ ...product, quantity: 1 });
     }
     updateCart();
+    Swal.fire({
+      text: "Regalo agregado al Carrito",
+      icon: "success",
+      showConfirmButton: false,
+      iconColor: "#dbc0cf",
+      timer: 2000,
+    });
   };
 
   window.removeFromCart = function (id) {
@@ -122,6 +126,9 @@ document.addEventListener("DOMContentLoaded", () => {
     cartModal.style.display = "none";
   });
 
+  acceptAndCloseButton.addEventListener("click", () => {
+    showAlertAndCloseModal();
+  });
 
   window.onclick = function (event) {
     if (event.target == cartModal) {
